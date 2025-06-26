5d697fdf987201f5b5153466cd5c43accc360142
ahamad236 <ahmadshaik4u@gmail.com>
2025-06-26 09:35:21 +0530

    change server index.js file

--- estore-server/index.js
+++ estore-server/index.js
@@ -1,20 +1,22 @@
-const express = require('express');
-const productCategories = require('./routes/productCategories');
-const products = require('./routes/products');
-const users = require('./routes/users');
+// To Start node.js server command : node index.js
+
+const express = require("express");
+const productCategories = require("./routes/productCategories");
+const products = require("./routes/products");
+const users = require("./routes/users");
 const app = express();
 const PORT = 5001;
-const cors = require('cors');
-const bodyparser = require('body-parser');
-const orders = require('./routes/orders');
+const cors = require("cors");
+const bodyparser = require("body-parser");
+const orders = require("./routes/orders");
 
 app.use(cors());
 app.use(bodyparser.json());
-app.use('/productCategories', productCategories);
-app.use('/products', products);
-app.use('/users', users);
-app.use('/orders', orders);
+app.use("/productCategories", productCategories);
+app.use("/products", products);
+app.use("/users", users);
+app.use("/orders", orders);
 
 const server = app.listen(PORT, () => {
-  console.log('App is running on the port - 5001');
+  console.log("App is running on the port - 5001");
 });
